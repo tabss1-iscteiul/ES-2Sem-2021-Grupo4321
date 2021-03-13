741c103ab22c882926d7a087b29da75e136617ba
tiagosil <tabss1@iscte-iul.pt>
2021-03-13 03:05:20 +0000

    excell

--- es/src/main/java/Trabalho/es/App.java
+++ es/src/main/java/Trabalho/es/App.java
@@ -1,13 +1,74 @@
 package Trabalho.es;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.FileOutputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class App {
+
+	public void writeExcell() {
+
+		try {
+			Workbook workbook = new XSSFWorkbook();
+			
+			Sheet sh = workbook.createSheet("Invoices");
+
+			String[] collumHeadings = { "Item id", "Item Name", "Qty", "Item Price", "Sold Date" };
+
+			Font headerFont = workbook.createFont();
+			headerFont.setBold(true);
+			headerFont.setFontHeightInPoints((short) 12);
+			headerFont.setColor(IndexedColors.BLACK.index);
+
+			CellStyle headerStyle = workbook.createCellStyle();
+			headerStyle.setFont(headerFont);
+			headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+			headerStyle.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.index);
+
+			Row headerRow = sh.createRow(0);
+
+			for (int i = 0; i < collumHeadings.length; i++) {
+				Cell cell = headerRow.createCell(i);
+				cell.setCellValue(collumHeadings[i]);
+				cell.setCellStyle(headerStyle);
+			}
+
+
+
+			for (int i = 0; i < collumHeadings.length; i++) {
+				sh.autoSizeColumn(i);
+			}
+			Sheet sh2 = workbook.createSheet("Second");
+
+			FileOutputStream fileOut = new FileOutputStream("C:\\Users\\TOSHIBA\\Documents\\teste\\invoices.xls");
+			workbook.write(fileOut);
+			fileOut.close();
+			workbook.close();
+			System.out.println("Completed");
+
+		} catch (Exception e) {
+
+		}
+
+	}
+
+	
+	
+	
+	public static void main(String[] args) {
+		new App().writeExcell();
+	}
 }
+
